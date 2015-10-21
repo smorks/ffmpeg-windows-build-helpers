836206232b8b9ecbbdf90ec88d9eeb54b09f6baf
Andy Brandt <andy@brandtware.com>
2015-10-21 09:58:08 -0500

    update to ffmpeg 2.8.1

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -1258,7 +1258,7 @@ build_ffmpeg() {
   local type=$1
   local shared=$2
   #local git_url="https://github.com/FFmpeg/FFmpeg.git"
-  local ffver="2.8"
+  local ffver="2.8.1"
   local output_dir="ffmpeg-$ffver"
   local ffurl="http://ffmpeg.org/releases/ffmpeg-${ffver}.tar.bz2"
 
