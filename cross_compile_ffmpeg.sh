05a295cb48b8df8f1afa993baeafea7f2fa78797
Andy Brandt <andy@brandtware.com>
2016-10-26 09:54:38 -0500

    update to ffmpeg 3.1.5

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -1462,7 +1462,7 @@ build_ffmpeg() {
   local shared_or_static=$1
   local extra_postpend_configure_options=$2
   #local git_url="https://github.com/FFmpeg/FFmpeg.git"
-  local ffver="3.1.4"
+  local ffver="3.1.5"
   local output_dir="ffmpeg-$ffver"
   local ffurl="http://ffmpeg.org/releases/ffmpeg-${ffver}.tar.bz2"
   #local output_dir=$3
